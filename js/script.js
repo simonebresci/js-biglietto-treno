b716ac9dd4b30f658ecdcbabb8d15aa6686ccad6
Simone <simone.bresci.05@gmail.com>
2020-12-14 15:05:37 +0100

    inizio progetto su layout preesistente

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Dichiarazione variabili
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65
+
+
+// // Debug
+// console.group('Debug');
+// console.log('Inizio del programma');
+//
+// // Acquisisci data dal sistemna
+// annoCorrente = new Date().getFullYear()
+//
+// // Acquisisci dati da utente
+// nome    = prompt('Qual è il tuo nome?');
+// cognome = prompt('Qual è il tuo cognome?');
+// colore  = prompt('Qual è il tuo colore preferito?');
+//
+// // Genera password
+// pass = nome + cognome + colore + annoCorrente;
+// console.log('La password generata è ' +pass);
+//
+// //Scrivi dati a video
+// document.getElementById('nome').innerText += " " + nome;
+// document.getElementById('cognome').innerText += " " + cognome;
+// document.getElementById('colore').innerText += " " + colore;
+// document.getElementById('password').innerText = pass;
+//
+//
+// console.log('Programma terminato con successo');
+// console.groupEnd('Debug');
+//  // /Debug
+
+function compra(){
+   alert('compro un biglietto!');
+ }
